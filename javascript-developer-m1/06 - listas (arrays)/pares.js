7ce09e94eb511584fe279824b242ba4fb0315d07
Ivaldo Paiva j® <123517102+IvaPaiva@users.noreply.github.com>
2024-10-12 15:16:32 -0300

    exercicio par

---
+++ javascript-developer-m1/06 - listas (arrays)/pares.js
@@ -0,0 +1,12 @@
+
+
+//2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
+
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} é par!`)
+        
+    }
+    
+    
+}
