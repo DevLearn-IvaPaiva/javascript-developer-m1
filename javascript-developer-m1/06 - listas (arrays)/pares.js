719721eb984303b76aa10a1f66ae49e63518a85a
Iva Paiva <123517102+IvaPaiva@users.noreply.github.com>
2024-10-12 15:24:01 -0300

    par impar

--- javascript-developer-m1/06 - listas (arrays)/pares.js
+++ javascript-developer-m1/06 - listas (arrays)/pares.js
@@ -2,11 +2,13 @@
 
 //2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i <= 50; i++) {
     if (i % 2 === 0) {
-        console.log(`${i} é par!`)
+            console.log(`${i} é par!`);
+        } else {
+            console.log(`${i} é impar!`);
+        }
         
     }
     
     
-}
